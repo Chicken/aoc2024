2d2572a552e03c0f75cbd086c2fad56ea8140ee7
Antti Ellilä <antti@antti.codes>
2024-12-10 07:45:05 +0200

    day10

---
+++ day10/part1.ts
@@ -0,0 +1,32 @@
+import type { Cell } from "../lib/Cell.ts";
+import { Grid } from "../lib/Grid.ts";
+import { Queue } from "../lib/Queue.ts";
+import { CellSet } from "../lib/Set.ts";
+
+const input = Deno.readTextFileSync("input").trim().split("\n").map((l) => l.trim().split("").map(Number));
+
+const grid = new Grid(input);
+
+let sum = 0;
+
+for (const [cell, value] of grid) {
+    if (value != 0) continue;
+    const heads = new CellSet();
+    const q = new Queue<Cell>();
+    q.enqueue(cell);
+    while (q.notEmpty()) {
+        const current = q.dequeue()!;
+        if (grid.get(current) == 9) {
+            heads.add(current);
+            continue;
+        }
+        for (const neighbour of grid.neighbours(current)) {
+            if (grid.get(neighbour) === grid.get(current) + 1) {
+                q.enqueue(neighbour);
+            }
+        }
+    }
+    sum += heads.size;
+}
+
+console.log(sum);
---
+++ day10/part2.ts
@@ -0,0 +1,31 @@
+import type { Cell } from "../lib/Cell.ts";
+import { Grid } from "../lib/Grid.ts";
+import { Queue } from "../lib/Queue.ts";
+
+const input = Deno.readTextFileSync("input").trim().split("\n").map((l) => l.trim().split("").map(Number));
+
+const grid = new Grid(input);
+
+let sum = 0;
+
+for (const [cell, value] of grid) {
+    if (value != 0) continue;
+    let heads = 0;
+    const q = new Queue<Cell>();
+    q.enqueue(cell);
+    while (q.notEmpty()) {
+        const current = q.dequeue()!;
+        if (grid.get(current) == 9) {
+            heads++;
+            continue;
+        }
+        for (const neighbour of grid.neighbours(current)) {
+            if (grid.get(neighbour) === grid.get(current) + 1) {
+                q.enqueue(neighbour);
+            }
+        }
+    }
+    sum += heads;
+}
+
+console.log(sum);
